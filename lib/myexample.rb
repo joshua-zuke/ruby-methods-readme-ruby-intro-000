e8dad698ec3e8b3a7ef89335e9abb456e2fbc2cd
Joshua Zuke <zuke.joshua@gmail.com>
2021-02-10 21:27:28 +0000

    Automatically backed up by Learn

---
+++ lib/myexample.rb
@@ -0,0 +1,13 @@
+def say_hello_ten_times
+  phrase = "Hellow World"
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+  puts phrase
+end
